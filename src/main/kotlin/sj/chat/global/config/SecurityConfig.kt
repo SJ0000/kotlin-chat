123d9bb2a363cf67d0add6471a6df0b77a943056
sj <slaayer60@gmail.com>
2023-12-27 16:34:03 +0900

    feat: Security config update

--- src/main/kotlin/sj/chat/global/config/SecurityConfig.kt
+++ src/main/kotlin/sj/chat/global/config/SecurityConfig.kt
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint
 
 @Configuration
 @EnableWebSecurity
@@ -13,7 +14,13 @@ class SecurityConfig(
 ) {
     @Bean
     fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http.csrf { csrf -> csrf.disable() }
+        http.headers { headers -> headers.frameOptions { frameOptions -> frameOptions.disable() } }
         http.formLogin { formLogin -> formLogin.disable() }
+
+        // RFC 7235에 정의된 WWW-Authenticate 헤더를 이용한 인증을 사용하지 않는다.
+        http.httpBasic { httpBasic -> httpBasic.disable() }
+
         return http.build()
     }
 }
