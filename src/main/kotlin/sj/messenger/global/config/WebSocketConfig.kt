28742f42ba35c0ef0e4fe505f07d60ec731b6506
sj <slaayer60@gmail.com>
2024-04-16 00:50:21 +0900

    refactor: websocket InboundChannel thread pool size 설정파일에서 로딩하도록 변경

--- src/main/kotlin/sj/messenger/global/config/WebSocketConfig.kt
+++ src/main/kotlin/sj/messenger/global/config/WebSocketConfig.kt
@@ -1,19 +1,24 @@
 package sj.messenger.global.config
 
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Configuration
 import org.springframework.messaging.simp.config.ChannelRegistration
 import org.springframework.messaging.simp.config.MessageBrokerRegistry
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor
 import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer
 
 @Configuration
 @EnableWebSocketMessageBroker
+@EnableConfigurationProperties(WebSocketProperties::class)
 class WebSocketConfig(
-    private val authenticationManager: AuthenticationManager,
+    private val properties: WebSocketProperties,
+    // private val authenticationManager: AuthenticationManager,
 ) : WebSocketMessageBrokerConfigurer{
 
     @Value("\${client.url}")
@@ -32,9 +37,14 @@ class WebSocketConfig(
 //        val authenticationInterceptor = AuthenticationChannelInterceptor(authenticationManager)
 //        registration.interceptors(authenticationInterceptor)
         val executor = ThreadPoolTaskExecutor()
-        executor.corePoolSize = Runtime.getRuntime().availableProcessors() * 2
-        executor.maxPoolSize = Runtime.getRuntime().availableProcessors() * 4
+        executor.corePoolSize = properties.corePoolSize
         executor.setAllowCoreThreadTimeOut(true)
         registration.taskExecutor(executor)
     }
 }
+
+@Validated
+@ConfigurationProperties("websocket")
+data class WebSocketProperties(
+    val corePoolSize: Int
+)
