d73e98ac3808703b3594ceb8245558af7b5665e2
sj <slaayer60@gmail.com>
2024-04-22 00:01:39 +0900

    feat: 성능 테스트를 위해 group message bypass 추가

--- src/main/kotlin/sj/messenger/domain/groupchat/controller/GroupChatStompController.kt
+++ src/main/kotlin/sj/messenger/domain/groupchat/controller/GroupChatStompController.kt
@@ -31,4 +31,19 @@ class GroupChatStompController(
         )
         template.convertAndSend("/topic/group-chat/${data.groupChatId}",data)
     }
+
+    @Timed("controller.group-chat-stomp.send-message-bypass")
+    @MessageMapping("/group-message/bypass")
+    fun sendMessageBypass(
+        @Payload sentGroupMessageDto: SentGroupMessageDto,
+    ) {
+        val data = ReceivedGroupMessageDto(
+            id = "not saved",
+            groupChatId = sentGroupMessageDto.groupChatId,
+            senderId = sentGroupMessageDto.senderId,
+            content = sentGroupMessageDto.content,
+            receivedAt = LocalDateTime.now()
+        )
+        template.convertAndSend("/topic/group-chat/bypass/${data.groupChatId}",data)
+    }
 }
