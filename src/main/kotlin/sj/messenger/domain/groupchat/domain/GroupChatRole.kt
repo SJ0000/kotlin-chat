03015b617185bf0ec1a329f243f7f8e5342b3ef3
sj <slaayer60@gmail.com>
2024-06-04 17:09:26 +0900

    feat: add GroupChatRole canEditTarget()

--- src/main/kotlin/sj/messenger/domain/groupchat/domain/GroupChatRole.kt
+++ src/main/kotlin/sj/messenger/domain/groupchat/domain/GroupChatRole.kt
@@ -1,5 +1,17 @@
 package sj.messenger.domain.groupchat.domain
 
+/*
+    ordinal 비교를 위해 높은 권한이 먼저 정의되어야 함.
+ */
 enum class GroupChatRole {
-    ADMIN, MODERATOR, MEMBER
+    ADMIN,
+    MODERATOR,
+    MEMBER;
+
+    fun canEditTarget(target: GroupChatRole): Boolean{
+        if(this == MEMBER)
+            return false
+
+        return this.ordinal < target.ordinal
+    }
 }
---
+++ src/test/kotlin/sj/messenger/domain/groupchat/domain/GroupChatRoleTest.kt
@@ -0,0 +1,40 @@
+package sj.messenger.domain.groupchat.domain
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class GroupChatRoleTest() {
+
+    @Test
+    @DisplayName("ADMIN과 MODERATOR는 자신보다 권한 등급이 낮은 target을 수정할 수 있다.")
+    fun canEditTarget() {
+        val admin = GroupChatRole.ADMIN
+        val moderator = GroupChatRole.MODERATOR
+        val member = GroupChatRole.MEMBER
+
+        assertThat(admin.canEditTarget(moderator)).isTrue()
+        assertThat(admin.canEditTarget(member)).isTrue()
+        assertThat(moderator.canEditTarget(member)).isTrue()
+    }
+
+    @Test
+    @DisplayName("같은 권한을 가진 target은 수정 할 수 없다.")
+    fun canEditTargetSameRole() {
+        val admin = GroupChatRole.ADMIN
+        val moderator = GroupChatRole.MODERATOR
+
+        assertThat(admin.canEditTarget(admin)).isFalse()
+        assertThat(moderator.canEditTarget(moderator)).isFalse()
+    }
+
+    @Test
+    @DisplayName("권한 등급이 MEMBER인 경우, 모든 target을 수정 할 수 없다.")
+    fun canEditTargetMember() {
+        val member = GroupChatRole.MEMBER
+
+        GroupChatRole.entries.forEach {
+            assertThat(member.canEditTarget(it)).isFalse()
+        }
+    }
+}
