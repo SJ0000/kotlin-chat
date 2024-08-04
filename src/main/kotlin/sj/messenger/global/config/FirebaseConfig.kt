e70f30b770410ca913dfedf2cd2316e5c447e17b
sj <slaayer60@gmail.com>
2024-08-04 15:41:57 +0900

    fix: Configuration @Value 오류 수정

--- src/main/kotlin/sj/messenger/global/config/FirebaseConfig.kt
+++ src/main/kotlin/sj/messenger/global/config/FirebaseConfig.kt
@@ -10,7 +10,7 @@ import org.springframework.core.io.ClassPathResource
 
 @Configuration
 class FirebaseConfig (
-    @Value("\${firebase.secretPath}")
+    @Value("\${firebase.secret-path}")
     val firebaseSecretPath: String,
 ){
 
